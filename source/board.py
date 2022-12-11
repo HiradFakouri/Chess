8d1b8ed867364879b1b4027a6e23b5f8ecdacef0
HiradFakouri <hiradfakouri200@gmail.com>
2022-12-11 20:09:30 +0000

    board on it

--- source/board.py
+++ source/board.py
@@ -3,5 +3,6 @@ import pygame
 class Board:
 
     def __init__(self):
-        self.image = pygame.image.load("../assets/board/board_plain_01.png")
+       self.image = pygame.image.load("assets/board/board_plain_01.png")
+       
         
--- source/pages.py
+++ source/pages.py
@@ -29,3 +29,5 @@ class Pages:
             for event in pygame.event.get(): 
                 if event.type == pygame.QUIT: 
                     running = False 
+            
+            draw()
